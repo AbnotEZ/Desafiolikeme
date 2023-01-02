0f8f6ac4f8f8829d4b34a890ef9f1b4e1b521341
Abnotez <feeer.andres@live.cl>
2023-01-02 02:59:26 -0300

    actualizacion de proyecto

--- index.js
+++ index.js
@@ -1,20 +1,39 @@
-const fs = require('fs');
-const express = require('express');
+const express = require("express");
 const app = express();
 
 app.use(express.json());
 
+const{ getAllPosts, createPost } = require("./services/posts");
 
-// middleware
+// MIDDLEWARE
 app.use(express.static(__dirname + "/public/static"));
+app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("estas solicitando la ruta raiz");
-    });
+  try {
+    res.send();
+  } catch (error) {
+    res.json({ message: "error 400 Bad Request" });
+  }
+});
 
-
-
-
-
+app.get("/posts", async (req, res) => {
+    try {
+      const getPosts = await getAllPosts();
+      res.json(getPosts);
+    } catch (error) {
+      console.log(error);
+    }
+  });
+  
+  app.post("/posts", async (req, res) => {
+    try {
+      const { titulo, url, descripcion } = req.body;
+      await createPost(titulo, url, descripcion);
+      res.send("Post creado");
+    } catch (error) {
+      console.log(error);
+    }
+  });
 
 app.listen(3000, console.log("SERVER ON 👌"));
